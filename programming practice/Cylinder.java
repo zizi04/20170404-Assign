8691957f4133317af7135a217e7245310c424bc2
zizi04 <disguy3939@naver.com>
2017-04-04 15:36:39 +0900

    4번문제
    4번문제

--- programming practice/Cylinder.java
+++ programming practice/Cylinder.java
@@ -1,13 +1,19 @@
 package programming;
 
 public class Cylinder {
+	Circle ci;
+	private double height;
 	
-	public static double getVoulme(Circle cir,double height){
-		return cir.getArea()*height;
+	public Cylinder(Circle ci, double height) {
+		this.ci = ci;
+		this.height = height;
+	}
+	
+	public static double getVoulme(double circleArea,double height){
+		return circleArea*height;
 	}
 	public static void main(String[] args){
-		Circle cir = new Circle(2.8);
-		double height=5.6;
-		System.out.println("반지름이 2.8,높이가 5.6인 원통부피: "+getVoulme(cir,height));
+		Cylinder cd = new Cylinder(new Circle(2.8), 5.6);
+		System.out.println("원통부피는 :"+cd.getVoulme(cd.ci.getArea(),cd.height));
 	}
 }
